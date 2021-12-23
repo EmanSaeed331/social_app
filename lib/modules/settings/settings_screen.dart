484e3a1fd2b6443819494a24abae79c8fbe5bbdc
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-23 03:27:00 +0200

    Fix: testing issue

--- lib/modules/settings/settings_screen.dart
+++ lib/modules/settings/settings_screen.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      children: [
+        Container(
+
+        ),
+      ],
+
+
     );
   }
 }
