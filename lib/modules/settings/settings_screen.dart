6847841dc9965d9556993634c7db6af4200723b8
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-23 04:02:42 +0200

    Fix:unsound flutter null safety issue

--- lib/modules/settings/settings_screen.dart
+++ lib/modules/settings/settings_screen.dart
@@ -7,43 +7,54 @@ class SettingsScreen extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          Stack(
-            children: [
-              Container(
-                height: 140,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(4.0),
-                    topRight: Radius.circular(4.0),
-                  ),
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      'https://image.freepik.com/free-photo/horizontal-shot-smiling-curly-haired-woman-indicates-free-space-demonstrates-place-your-advertisement-attracts-attention-sale-wears-green-turtleneck-isolated-vibrant-pink-wall_273609-42770.jpg',
+          Container(
+            height: 180,
+            child: Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                Align(
+
+                  child: Container(
+                    height: 140,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          'https://image.freepik.com/free-photo/horizontal-shot-smiling-curly-haired-woman-indicates-free-space-demonstrates-place-your-advertisement-attracts-attention-sale-wears-green-turtleneck-isolated-vibrant-pink-wall_273609-42770.jpg',
+                        ),
+                        fit:BoxFit.cover,
+
+
+                      ),
                     ),
-                    fit:BoxFit.cover,
-
-
                   ),
-                ),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                ),
-                child:  CircleAvatar(
-                  radius: 18.0,
-                  backgroundImage: NetworkImage(
-                    'https://image.freepik.com/free-photo/horizontal-shot-smiling-curly-haired-woman-indicates-free-space-demonstrates-place-your-advertisement-attracts-attention-sale-wears-green-turtleneck-isolated-vibrant-pink-wall_273609-42770.jpg',
-                  ),
+                  alignment: AlignmentDirectional.topCenter,
                 ),
 
-              ),
-            ],
+                 CircleAvatar(
+                   radius: 54,
+                   backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                   child: CircleAvatar(
+                      radius: 50.0,
+                      backgroundImage: NetworkImage(
+                        'https://image.freepik.com/free-photo/horizontal-shot-smiling-curly-haired-woman-indicates-free-space-demonstrates-place-your-advertisement-attracts-attention-sale-wears-green-turtleneck-isolated-vibrant-pink-wall_273609-42770.jpg',
+                      ),
+                    ),
+                 ),
+
+              ],
+            ),
           ),
+          Text('Eman Saeed',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          Text('bio ...',
+            style: Theme.of(context).textTheme.caption,
+          )
 
         ],
 
