00930025886df35b9bbba25a42de4f2925ee27d5
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-26 04:19:32 +0200

    Feature:Add post design completed

--- lib/modules/new_post/new_post_screen.dart
+++ lib/modules/new_post/new_post_screen.dart
@@ -1,14 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:socialapp/modules/social_layout/cubit/cubit.dart';
+import 'package:socialapp/modules/social_layout/cubit/states.dart';
 import 'package:socialapp/shared/components/components.dart';
+import 'package:socialapp/shared/style/icon_broken.dart';
 class NewPostScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:defaultAppBar(
-        context: context,
-        title: 'Add post',
-      ),
+    return BlocConsumer<SocialCubit,SocialStates>(
+      listener: (context,state){},
+      builder: (context,state){
+        return Scaffold(
+          appBar:defaultAppBar(
+            context: context,
+            title: 'Create Post',
+            actions: [
+              defaultTextButton(
+                  function: (){},
+                  text: 'Post')
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 25.0,
+                      backgroundImage: NetworkImage(
+                        'https://image.freepik.com/free-photo/horizontal-shot-smiling-curly-haired-woman-indicates-free-space-demonstrates-place-your-advertisement-attracts-attention-sale-wears-green-turtleneck-isolated-vibrant-pink-wall_273609-42770.jpg',
+                      ),
+                    ),
+                    SizedBox(width: 15,),
+                    Expanded(
+                      child: Text(
+                        'Eman Saeed',
+                        style: TextStyle(
+                            height:1.4
+                        ),
+                      ),
+
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'What is on your mind ...',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                          onPressed: (){},
+                          child:
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                IconBroken.Image,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                'Add photo'
+                              ),
+                            ],
+
+                          ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: (){},
+                        child:
+                        Text(
+                            '# tags'
+                        ),
+                      ),
+                    ),
+
+
+
+                  ],
+                ),
+              ],
+            ),
+
+          ),
+
+        );
+      },
 
     );
   }
