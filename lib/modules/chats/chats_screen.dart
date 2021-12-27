509263d69b7248231b739909b65d251c5cf26cb5
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2021-12-28 00:14:41 +0200

    Feature:Chat design added

--- lib/modules/chats/chats_screen.dart
+++ lib/modules/chats/chats_screen.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:socialapp/modules/social_layout/cubit/cubit.dart';
+import 'package:socialapp/modules/social_layout/cubit/states.dart';
+import 'package:socialapp/shared/components/components.dart';
+import 'package:socialapp/shared/style/colors.dart';
 
 class ChatsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Chats Screen');
+    return BlocConsumer<SocialCubit,SocialStates>(
+      listener: (context,state){},
+      builder: (context,state){
+        return  ListView.separated(
+          physics: BouncingScrollPhysics(),
+            itemBuilder: (context,index)=>builChatItem(),
+            separatorBuilder: (context,index)=> myDivider(),
+            itemCount: 10);
+      },
+
+    );
+
   }
+  Widget builChatItem () => InkWell(
+    onTap: (){},
+    child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 25.0,
+            backgroundImage: NetworkImage(
+                'https://image.freepik.com/free-photo/image-young-business-woman-apologizing-shrugging-shoulders-looking-confused-dont-know-standing-white_176420-41076.jpg'          ),
+          ),
+          SizedBox(width: 15,),
+          Text(
+            'Eman Saeed',
+            style: TextStyle(
+                height:1.4
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
